5497f6d915443ee66249580492fb569440788e1f
Janarthanan Kannan <tjana17@gmail.com>
2025-11-30 19:13:50 +0530

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Janarthanan Kannan on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
