09409822ccfb12c240eb3a256abc596b0486154c
Janarthanan Kannan <tjana17@gmail.com>
2025-12-19 19:54:21 +0530

    Added Login, Forgot Password screens and app icon

---
+++ Expense Tracker/Views/Authentication/ForgotPasswordSheet.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordSheet.swift
+//  Expense Tracker
+//
+//  Created by Janarthanan Kannan on 19/12/25.
+//
+
+import SwiftUI
+
+struct ForgotPasswordSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordSheet()
+}
